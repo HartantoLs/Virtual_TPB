9bcc8d0288f8ddd17b17b93b3767daee1ff333ff
HartantoLs <117240954+HartantoLs@users.noreply.github.com>
2024-11-15 00:20:15 +0700

    Update index.js

--- backEnd/index.js
+++ backEnd/index.js
@@ -30,18 +30,17 @@ app.use(cors({
 const { Client } = pg;
 const db = new Client({
     connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false // SSL configuration for Neon PostgreSQL
-    }
+    ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
 });
 
-db.connect()
-    .then(() => {
+(async () => {
+    try {
+        await db.connect();
         console.log("Connected to the database successfully!");
-    })
-    .catch((err) => {
+    } catch (err) {
         console.error("Failed to connect to the database:", err.stack);
-    });
+    }
+})();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -55,12 +54,14 @@ app.use(session({
 
 async function saveHistory(user_id, action) {
     const timestamp = new Date();
-    await db.query("INSERT INTO history (user_id, action, timestamp) VALUES ($1, $2, $3)", [user_id, action, timestamp]);
+    try {
+        await db.query("INSERT INTO history (user_id, action, timestamp) VALUES ($1, $2, $3)", [user_id, action, timestamp]);
+    } catch (err) {
+        console.error("Failed to save history:", err);
+    }
 }
 
-
 app.get("/", (req, res) => {
-    // console.log("lol")
     res.sendFile(path.join(__dirname, "..", "frontEnd", "login", "home.html"));
 });
 app.use(express.static(path.join(__dirname, "..", "frontEnd")));
@@ -69,17 +70,15 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "frontEnd", "login", "login.html"));
 });
 
-
 app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "..", "frontEnd", "login", "register.html"));
 });
-
 
 function authenticateSession(req, res, next) {
     if (req.session.user) {
         next();
     } else {
-        res.status(401).redirect("/login"); // Mengarahkan pengguna ke halaman login jika sesi tidak ada
+        res.status(401).redirect("/login");
     }
 }
 
@@ -102,13 +101,12 @@ app.post("/register", async (req, res) => {
             res.redirect("/login");
         }
     } catch (err) {
-        console.log(err);
+        console.error("Error during registration:", err);
+        res.status(500).json({ error: "Internal server error" });
     }
 });
 
-
 app.post("/login", async (req, res) => {
-    console.log("Data yang diterima di req.body:", req.body);
     const email = req.body.username;
     const password = req.body.password;
 
@@ -119,7 +117,6 @@ app.post("/login", async (req, res) => {
             const user = result.rows[0];
             const storedPassword = user.password;
 
-            // Cocokkan password
             if (password === storedPassword) {
                 req.session.user = { id: user.id, email: user.email }; // Simpan data user di sesi
                 await saveHistory(user.id, "Login");
@@ -131,7 +128,7 @@ app.post("/login", async (req, res) => {
             res.status(404).json({ error: "User not found" });
         }
     } catch (err) {
-        console.error(err);
+        console.error("Error during login:", err);
         res.status(500).json({ error: "Internal server error" });
     }
 });
@@ -142,44 +139,32 @@ app.get("/history", authenticateSession, async (req, res) => {
         const result = await db.query("SELECT * FROM history WHERE user_id = $1 ORDER BY timestamp DESC", [user_id]);
         res.json(result.rows);
     } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: "Gagal mengambil riwayat" });
+        console.error("Error fetching history:", err);
+        res.status(500).json({ error: "Failed to fetch history" });
     }
 });
 
-app.post("/log-action", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).json({ error: "Unauthorized" });
-    }
-
+app.post("/log-action", authenticateSession, async (req, res) => {
     const user_id = req.session.user.id;
     const action = req.body.action || "Aksi yang tidak dijelaskan";
-    console.log("user id : ", user_id);
-    console.log("aksi yang dilakukan : ", action)
 
     try {
         await saveHistory(user_id, action);
         res.json({ message: "Action logged successfully" });
     } catch (err) {
-        console.log(err);
+        console.error("Error logging action:", err);
         res.status(500).json({ error: "Failed to log action" });
     }
-    });
+});
 
-    app.get("/get-user-data", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).json({ error: "Unauthorized" });
-    }
-
+app.get("/get-user-data", authenticateSession, async (req, res) => {
     try {
-        
         const userResult = await db.query("SELECT email FROM users WHERE id = $1", [req.session.user.id]);
 
         if (userResult.rows.length === 0) {
             return res.status(404).json({ error: "User not found" });
         }
 
-        
         res.json({ username: userResult.rows[0].email });
     } catch (error) {
         console.error("Error fetching user data:", error);
@@ -187,20 +172,13 @@ app.post("/log-action", async (req, res) => {
     }
 });
 
-
-app.get("/user-history", async (req, res) => {
-    if (!req.session.user) {
-        return res.status(401).json({ error: "Unauthorized" });
-    }
-
+app.get("/user-history", authenticateSession, async (req, res) => {
     try {
-        
         const historyResult = await db.query(
             "SELECT action, timestamp FROM history WHERE user_id = $1 ORDER BY timestamp DESC LIMIT 30", 
             [req.session.user.id]  
         );
 
-    
         res.json(historyResult.rows);
     } catch (error) {
         console.error("Error fetching user history:", error);
@@ -208,25 +186,16 @@ app.get("/user-history", async (req, res) => {
     }
 });
 
-
-app.post("/change-password", async (req, res) => {
+app.post("/change-password", authenticateSession, async (req, res) => {
     const { oldPassword, newPassword } = req.body;
 
-    
-    if (!req.session.user) {
-        return res.status(401).json({ error: "Unauthorized" });
-    }
-
     try {
-        
         const userResult = await db.query("SELECT password FROM users WHERE id = $1", [req.session.user.id]);
 
-        
         if (userResult.rows.length === 0 || userResult.rows[0].password !== oldPassword) {
             return res.status(400).json({ message: "Incorrect old password" });
         }
 
-        
         await db.query("UPDATE users SET password = $1 WHERE id = $2", [newPassword, req.session.user.id]);
         await saveHistory(req.session.user.id, "Password changed");
         
@@ -237,7 +206,6 @@ app.post("/change-password", async (req, res) => {
     }
 });
 
-// Endpoint untuk Logout
 app.post("/logout", (req, res) => {
     req.session.destroy((err) => {
         if (err) {
@@ -248,16 +216,12 @@ app.post("/logout", (req, res) => {
     });
 });
 
-
-// game history //
-
 // Endpoint untuk menyimpan hasil permainan
 app.post("/save-game-result", authenticateSession, async (req, res) => {
     const { result, distance_to_finish } = req.body;
-    const user_id = req.session.user.id; // Ambil user_id dari session
+    const user_id = req.session.user.id;
 
     try {
-        // Menyimpan hasil permainan ke database
         await db.query("INSERT INTO game_results (user_id, result, distance_to_finish, timestamp) VALUES ($1, $2, $3, NOW())", [user_id, result, distance_to_finish]);
         res.json({ message: "Game result saved successfully" });
     } catch (error) {
@@ -268,7 +232,7 @@ app.post("/save-game-result", authenticateSession, async (req, res) => {
 
 // Endpoint untuk mengambil riwayat permainan
 app.get("/user-game-history", authenticateSession, async (req, res) => {
-    const user_id = req.session.user.id; // Ambil user_id dari session
+    const user_id = req.session.user.id;
 
     try {
         const result = await db.query("SELECT result, distance_to_finish, timestamp FROM game_results WHERE user_id = $1 ORDER BY timestamp DESC LIMIT 30", [user_id]);
